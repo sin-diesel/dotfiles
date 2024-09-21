30e393c0f898b55fd244ba3cf48b58c2ffd8f125
Stanislav <sidelnikov.si.001@gmail.com>
2024-09-21 13:45:05 +0300

    Add fish and vim installations and setups

--- install.sh
+++ install.sh
@@ -3,28 +3,37 @@
 set -e
 set -x
 
-cd $HOME
-
 # gogh installation
 echo "Setting up terminal theme with gogh..."
-if [ -d "gogh" ]; then
+if [ -d $HOME/gogh ]; then
   echo "Found existing gogh installation at "$HOME"/gogh. Skipping repo clone..."
 else
-  git clone https://github.com/Gogh-Co/Gogh.git gogh
+  git clone https://github.com/Gogh-Co/Gogh.git $HOME/gogh
 fi
 
 # necessary in the Gnome terminal on ubuntu
 export TERMINAL=gnome-terminal
 
-./gogh/installs/belafonte-day.sh
+$HOME/gogh/installs/belafonte-day.sh
 
 # fish installation
 echo " Setting up fish shell..."
 sudo apt install fish
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
-fish install --offline=omf.tar.gz
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish || true
 
-omf theme bobthefish
+fish -c "omf theme bobthefish"
+cp -r fish $HOME/.config/fish/
+
+# set up vim
+echo "Setting up vim..."
+if [ -d $HOME/.vim/bundle/Vundle.vim ]; then
+  echo "Found existing Vundle installation at $HOME/.vim/bundle/Vundle.vim. Skipping Vundle install..."
+else
+  git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
+
+cp vim/.vimrc $HOME/.vimrc
+vim +PluginInstall +qall
 
 # cleanup
 rm -rf gogh
